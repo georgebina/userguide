ddeaf1b0d09a54963fa3da2408baacb9d61718e0
Cristian Talau <cristitalau@gmail.com>
2016-05-05 20:22:11 +0000

    rebuild pages at bf97701

---
+++ oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,16 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "topics/browser-compatibility.html";
+fl["1"]= "topics/checking-for-errors.html";
+fl["2"]= "topics/copyright.html";
+fl["3"]= "topics/editingActions.html";
+fl["4"]= "topics/how_to_webapp_webdav.html";
+fl["5"]= "topics/license_issues.html";
+fl["6"]= "topics/perform_common_review_actions.html";
+fl["7"]= "topics/performing-common-actions.html";
+fl["8"]= "topics/using-the-webapp-reviewer.html";
+fl["9"]= "topics/using-webapp-github.html";
+fl["10"]= "topics/webapp-dashboard.html";
+fl["11"]= "topics/webapp-overview.html";
+fl["12"]= "topics/webapp_smart_paste.html";
+var doStem = false;searchLoaded = true;
